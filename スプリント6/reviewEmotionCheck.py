3393a61762554d072a0eff3e576f9157050390b6
takashi.torii.dc <takashi.torii.dc>
2024-09-20 16:47:55 +0900

    update reviewEmotionCheck

--- スプリント6/reviewEmotionCheck.py
+++ スプリント6/reviewEmotionCheck.py
@@ -2,39 +2,29 @@ import json
 
 import boto3
 
-# --------------------
-# DynamoDBから口コミを取得
-# --------------------
+# パフォーマンスの向上、コスト削減目的でDynamoDBとComprehendクライアントをグローバルで定義
+dynamodb = boto3.resource('dynamodb')
+comprehend = boto3.client('comprehend')
+
 def fetch_review_text_from_dynamodb(review_id):
-    # DynamoDBのクライアントを作成
-    dynamodb = boto3.resource('dynamodb')
-    
     # DynamoDBのテーブルを指定
     table = dynamodb.Table('reviews')
-    
     # DynamoDBからレビューテキストを取得
     response = table.get_item(Key={'id': review_id})
+    if 'Item' not in response:
+        return None  # レビューが見つからない場合
     review_text = response['Item']['reviewText']
-    
+
     return review_text
 
-# --------------------
-# 感情分析を実施
-# --------------------
+# レビューの感情分析
 def analyze_sentiment(review_text):
-    # Amazon Comprehendで感情分析
-    comprehend = boto3.client('comprehend')
-    analysis_result = comprehend.detect_sentiment(Text=review_text, LanguageCode='ja')
+    if not review_text:
+        return None
+    return comprehend.detect_sentiment(Text=review_text, LanguageCode='ja')["Sentiment"]
 
-    return analysis_result["Sentiment"]
-
-# --------------------
-# 感情分析の結果を保存
-# --------------------
+# レビューのレコードに感情分析結果を更新して追加
 def update_review_sentiment_in_dynamodb(review_id, review_sentiment):
-    # DynamoDBのクライアントを作成
-    dynamodb = boto3.resource('dynamodb')
-    
     # DynamoDBのテーブルを指定
     table = dynamodb.Table('reviews')
     
@@ -47,16 +37,18 @@ def update_review_sentiment_in_dynamodb(review_id, review_sentiment):
         }
     )
 
-# --------------------
 # メイン関数
-# --------------------
 def lambda_handler(event, context):
-    
     # イベントからidを取得
     review_id = event['id']
-    
     # レビューテキストを取得
     review_text = fetch_review_text_from_dynamodb(review_id)
+    # テキストがない場合エラーをリターン
+    if review_text is None:
+        return {
+            'statusCode': 404,
+            'body': json.dumps('Review not found')
+        }
     
     # 取得したレビューをcomprehendで分析し感情を取得
     review_sentiment = analyze_sentiment(review_text)
